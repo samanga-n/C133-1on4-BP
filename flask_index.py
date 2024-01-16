ed7e05da2fca2b42e893a20bbe852b90b22c17f0
Samanga Narayanan <74295785+samanga-n@users.noreply.github.com>
2024-01-16 19:59:02 +0530

    Update flask_index.py

--- flask_index.py
+++ flask_index.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 #in the function return render_template(‘index.html’)
-@app.route("/index")
-def first_webpage():
+@app.route("/")
+def first_flask():
     #Create a variable
     name = 'Flask'
     # Pass the variable in the template
